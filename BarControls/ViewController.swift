1d44ec15d9d050cb3e550863a852fa4ad0b71d9a
slawa <slawa@slawagurevich.com>
2020-02-21 18:40:58 +0100

    Initial Commit

---
+++ BarControls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BarControls
+//
+//  Created by Slawa on 21.02.20.
+//  Copyright © 2020 slawa.gurevich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
